c74696ae298e4233b835a49bf88930cae1035186
Athar Javed <atharjaved15@gmail.com>
2020-11-25 20:44:11 -0800

    Done: update Color

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'repeatedCodes.dart';
 
+const activeColor =  Color(0xFF1D1E33);
+const deactiveColor = Color(0xFF111328);
+
 
 class InputPage extends StatefulWidget {
   @override
@@ -9,6 +12,18 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleColor = deactiveColor;
+  Color femaleColor = deactiveColor;
+  void updateColor(int gender){
+    if(gender == 1){
+      maleColor = activeColor;
+      femaleColor = deactiveColor;
+    }
+    if(gender == 2){
+      maleColor = deactiveColor;
+      femaleColor = activeColor;
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
