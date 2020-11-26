02154db6acb1de7db2d7b7537b2fc17bf948a598
Athar Javed <atharjaved15@gmail.com>
2020-11-25 19:21:02 -0800

    Done: Layout Input Page

---
+++ lib/input_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+
+      ),
+      body: Column(
+        children: <Widget> [
+          Expanded(child: Row(
+            children: <Widget>[
+              Expanded(child: null),
+              Expanded(child: null),
+
+            ],
+          )),
+          Expanded(child: null),
+          Expanded(child:  Row(
+            children: <Widget>[
+              Expanded(child: null),
+              Expanded(child: null),
+
+            ],
+          )),
+
+        ],
+      ),
+    );
+  }
+}
