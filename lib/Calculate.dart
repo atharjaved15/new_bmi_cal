51d1acabfbdcebc14d83eb621904cc30e5c747d9
Athar Javed <atharjaved15@gmail.com>
2020-11-26 00:12:01 -0800

    Done: Calculate File

--- lib/Calculate.dart
+++ lib/Calculate.dart
@@ -7,6 +7,33 @@ class CalculatorBrain{
 
   final int height;
   final int weight;
-  double BMI;
+  double bmi;
+
+  String CalculateBMI(){
+  bmi = weight / pow(height/100 , 2);
+  return bmi.toStringAsFixed(1);
+  }
+
+  String getResult () {
+    if(bmi >18.5){
+      return 'NORMAL';
+    }
+    else{
+      return 'UNDERWEIGHT';
+    }
+  }
+
+  String getInterpretation()
+  {
+    if(bmi>25){
+      return 'You have a higher than normal body weight. Try to Exercise';
+    }
+    else if(bmi>18){
+      return 'You have a normal body weight. Good Job !';
+    }
+    else{
+      return 'You have a lower than normal body weight. You can eat a bit more';
+    }
+  }
 
 }
