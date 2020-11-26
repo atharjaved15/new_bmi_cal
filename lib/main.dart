f3c3c1354e1c61d0ef3dbbaf2809083631713fab
Athar Javed <atharjaved15@gmail.com>
2020-11-25 19:10:52 -0800

    Done Lecture 55: Theme Data Widget. and Seprate Class for Inout Page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(BMICalculator());
 }
 
-class MyApp extends StatelessWidget {
+class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home:InputPage(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      )
 
     );
   }
 }
+
+
+
 
