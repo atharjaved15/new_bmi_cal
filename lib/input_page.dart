e42831558aa5bc725d8764bd49d187e68b837613
Athar Javed <atharjaved15@gmail.com>
2020-11-25 19:25:53 -0800

    Done: Layout Input Page with Styles

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,18 +16,54 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Column(
         children: <Widget> [
-          Expanded(child: Row(
+          Expanded(child:
+          Row(
             children: <Widget>[
-              Expanded(child: null),
-              Expanded(child: null),
+              Expanded(child:
+              Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              )
+              ),
+              Expanded(child:
+              Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              )),
 
             ],
           )),
-          Expanded(child: null),
-          Expanded(child:  Row(
+          Expanded(child:
+          Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF1D1E33),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          )),
+          Expanded(child:
+          Row(
             children: <Widget>[
-              Expanded(child: null),
-              Expanded(child: null),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              )),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              )),
 
             ],
           )),
